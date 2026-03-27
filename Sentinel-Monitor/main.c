7f902896dbefd6d21f8b4da3b288f20cb9320909
Morgana-idk <pedrokook123p@gmail.com>
2026-03-27 20:16:14 -0300

    adiciona loop infinito no Sentinel Monitor e cria Makefile para compilação

--- Sentinel-Monitor/main.c
+++ Sentinel-Monitor/main.c
@@ -2,6 +2,11 @@
 
 int main() {
     
+    while (1){
+        printf("\033[H\033[J");
+        printf("\n\n==========SENTINEL MONITOR==========\n");
+        printf("====================================\n\n");
+    }
     
 
     return 0;
