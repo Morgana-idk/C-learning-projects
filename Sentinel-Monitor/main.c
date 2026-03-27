1c0846c2f192f59e800fe050805eb975a666d40b
Morgana-idk <pedrokook123p@gmail.com>
2026-03-27 20:19:45 -0300

    adiciona contagem e exibição no loop do Sentinel Monitor

--- Sentinel-Monitor/main.c
+++ Sentinel-Monitor/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
     
+    int contagem = 0;
+
     while (1){
         printf("\033[H\033[J");
         printf("\n\n==========SENTINEL MONITOR==========\n");
+        printf("%d\n", contagem);
         printf("====================================\n\n");
+        contagem++;
+        sleep(1);
     }
     
 
