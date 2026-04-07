48b9913915016173612d8c3eb8e4d7c3b4210db5
Morgana-idk <pedrokook123p@gmail.com>
2026-04-06 21:52:20 -0300

    Adiciona sistema de inventário com gravação em arquivo e ajusta lógica de execução

--- Mini-Block-Man/inventory-system/inventory.c
+++ Mini-Block-Man/inventory-system/inventory.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 int main(){
+    FILE *invtxt = fopen("/home/modulescript/projetos/C-learning-projects/Mini-Block-Man/inv.txt", "w");
     struct inventory{
         char slots[5][50];
     };
@@ -10,9 +11,12 @@ int main(){
     for (int i = 0; i < 5; i++){
         if (strcmp(inv.slots[i], "null") != 0 && inv.slots[i][0] != '\0'){
             printf("existe\n");
+            char item[50];
+            sprintf(item, "%s\n", inv.slots[i]);
+            fputs(item, invtxt);
         } else {
             printf("n existe\n");
-            return 1;
+            
         }
     }
     return 0;
