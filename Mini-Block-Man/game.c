69393a1b32312c21ec56f064ef0f8162d2e73094
Morgana-idk <pedrokook123p@gmail.com>
2026-04-08 20:54:09 -0300

    Adiciona função de atualização do inventário e lógica para ativação com a tecla 'I'

--- Mini-Block-Man/game.c
+++ Mini-Block-Man/game.c
@@ -3,8 +3,17 @@
 #include "raylib.h"
 #include <unistd.h>
 
+void update_inv(){
+    char diretorio_atual[150];
+    char diretorio_inv[] = "inventory-system";
+    char comando[200];
+    sprintf(comando, "cd %s && make && cd %s", diretorio_inv, diretorio_atual);
+    getcwd(diretorio_atual, sizeof(diretorio_atual));
+    system(comando);
+};
+
 int main() {
-    // Configurações
+    update_inv();
     float speed = 300.0f;
     float gravity = 1500.0f;    // Valor mais suave para o pulo não ser bizarro
     float jumppower = -600.0f;  // Negativo porque no Y da tela, para cima é menos
@@ -14,17 +23,20 @@ int main() {
     InitWindow(800, 450, "Mini Block Man - Corrigido!");
     SetTargetFPS(60);
 
+    
     Rectangle player = { 400, 200, 40, 40 };
     Rectangle floor = { 0, 410, 800, 40 };
+    
 
     while (!WindowShouldClose()) {
-        char diretorio_atual[150];
-        char diretorio_inv[] = "inventory-system";
-        char comando[200];
-        sprintf(comando, "cd %s && make && cd %s", diretorio_inv, diretorio_atual);
-        getcwd(diretorio_atual, sizeof(diretorio_atual));
-        system(comando);
-        
+        if (IsKeyDown(KEY_I)){
+            update_inv();
+        }
+        Rectangle slot1 = { player.x + 1, floor.y + 1, 38, 38};
+        Rectangle slot2 = { player.x + (slot1.width + 5), floor.y + 1, 38, 38};
+        Rectangle slot3 = { player.x + (slot2.width * 2) + 10, floor.y + 1, 38, 38};
+        Rectangle slot4 = { player.x - slot2.width - 5, floor.y + 1, 38, 38};
+        Rectangle slot5 = { player.x - (slot4.width * 2) - 10, floor.y + 1, 38, 38};
         float frametime = GetFrameTime();
 
         // 1. Movimento Horizontal
@@ -32,7 +44,7 @@ int main() {
         if (IsKeyDown(KEY_A)) player.x -= speed * frametime;
 
         // 2. Lógica de Pulo (Só pula se estiver no chão)
-        if (IsKeyPressed(KEY_SPACE) && tocandochao) {
+        if (IsKeyPressed(KEY_SPACE) && tocandochao || IsKeyPressed(KEY_W) && tocandochao) {
             velocityY = jumppower;
             tocandochao = false;
         }
@@ -59,6 +71,11 @@ int main() {
             ClearBackground(RAYWHITE);
             DrawRectangleRec(floor, GREEN);
             DrawRectangleRec(player, RED);
+            DrawRectangleRec(slot1, GRAY);
+            DrawRectangleRec(slot2, GRAY);
+            DrawRectangleRec(slot3, GRAY);
+            DrawRectangleRec(slot4, GRAY);
+            DrawRectangleRec(slot5, GRAY);
             DrawText("ESPAÇO para pular!", 10, 10, 20, DARKGRAY);
         EndDrawing();
     }
